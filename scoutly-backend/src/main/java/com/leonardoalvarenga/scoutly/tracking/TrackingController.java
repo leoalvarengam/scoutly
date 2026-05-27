19b719862a63221e941fc12e02fe329e3271888e
ynfleozin <leonardoalvarengam@gmail.com>
2026-05-27 15:30:29 -0300

    feat: add tracked product controller

---
+++ scoutly-backend/src/main/java/com/leonardoalvarenga/scoutly/tracking/TrackingController.java
@@ -0,0 +1,41 @@
+package com.leonardoalvarenga.scoutly.tracking;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/tracking")
+@RequiredArgsConstructor
+public class TrackingController {
+    private final TrackingService service;
+
+    @PostMapping
+    public ResponseEntity<TrackingResponseDTO> create(@Valid @RequestBody TrackingRequestDTO dto){
+        TrackingResponseDTO response = service.add(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TrackingRequestDTO>> get(){
+        List<TrackingRequestDTO> productsList = service.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(productsList);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<TrackedProduct> deactivateAlert(@PathVariable UUID id){
+        service.deactivateAlert(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
