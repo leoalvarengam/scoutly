d246c3d9ea2f702a5ad92254f5881c8898fe3583
ynfleozin <leonardoalvarengam@gmail.com>
2026-06-22 18:35:02 -0300

    feat: add landing and login pages with new routes

--- scoutly-frontend/src/app/app.routes.ts
+++ scoutly-frontend/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { TrackedProductsComponent } from './pages/tracked-products/tracked-products.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
+    component: LandingComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'dashboard',
     component: TrackedProductsComponent,
   },
   {
