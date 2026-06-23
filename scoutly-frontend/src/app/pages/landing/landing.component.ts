807150457d6a8279bc3e203d8387207ff44aaa2a
ynfleozin <leonardoalvarengam@gmail.com>
2026-06-23 19:14:00 -0300

    feat: implement landing and login page layout and styles

--- scoutly-frontend/src/app/pages/landing/landing.component.ts
+++ scoutly-frontend/src/app/pages/landing/landing.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.scss'
+  styleUrl: './landing.component.scss',
 })
-export class LandingComponent {
-
-}
+export class LandingComponent {}
